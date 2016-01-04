aad61381c7f4cf85a15f58a9aea90a3775043ec2
abhi-1309 <tech.abhishek137@gmail.com>
2016-01-04 17:04:18 +0530

    library page

--- resources/views/library.blade.php
+++ resources/views/library.blade.php
@@ -2,46 +2,127 @@
 <html>
 <head>
 	 <!-- <link  href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet"> -->
-	 <link href="css/style.css" rel="stylesheet">
+	 <link href="css/button.css" rel="stylesheet">
+	 <link href="css/panel.css" rel="stylesheet">
+	 <link href="css/library.css" rel="stylesheet">
+
 </head>
 
 <body>
-<div class="col-md-8 col-md-offset-2">
-<div class="panel panel-primary">
- <div class="panel-heading">
-		<h2 class="panel-title text-center" >
-			<strong> Button library</strong></br>
-		</h2>
+	<div class="row">
+		<div class="col-md-12" style="background-color: #5F9EA0" align="center">
+			 <h1>e-Yantra library</h1>
 		</div>
-			<div>
-			<button type="button" class="btn btn-eyrc1">color-1</button></br></br>
+	</div>
 
-			<button type="button" class="btn btn-eyrc13">color-13</button></br></br>
-
-			<button type="button" class="btn btn-eyrc2">color-2</button></br></br>
-
-			<button type="button" class="btn btn-eyrc3">color-3</button></br></br>
-
-			<button type="button" class="btn btn-eyrc4">color-4</button></br></br>
-
-			<button type="button" class="btn btn-eyrc5">color-5</button></br></br>
-
-			<button type="button" class="btn btn-eyrc6">color-6</button></br></br>
-
-			<button type="button" class="btn btn-eyrc7">color-7</button></br></br>
-
-			<button type="button" class="btn btn-eyrc8">color-8</button></br></br>
-
-			<button type="button" class="btn btn-eyrc9">color-9</button></br></br>
-
-			<button type="button" class="btn btn-eyrc10">color-10</button></br></br>
-
-			<button type="button" class="btn btn-eyrc11">color-11</button></br></br>
-
-			<button type="button" class="btn btn-eyrc12">color-12</button></br></br>
-
-			<button type="button" class="btn btn-eyrc14">color-14</button></br></br>
+	<div class="row">
+		<div class="col-md-4">
+			<h2>Button</h2> 
+				<button type="button" class="btn btn-eyrc1">.btn-eyrc1</button>&nbsp;
+				<button type="button" class="btn btn-eyrc13">.btn-eyrc13</button>&nbsp;
+				<button type="button" class="btn btn-eyrc3">.btn-eyrc3</button></br></br>
+				<button type="button" class="btn btn-eyrc2">.btn-eyrc2</button>&nbsp;
+				<button type="button" class="btn btn-eyrc4">.btn-eyrc4</button>&nbsp;
+				<button type="button" class="btn btn-eyrc14">.btn-eyrc14</button></br></br>
+				<button type="button" class="btn btn-eyrc5">.btn-eyrc5</button>&nbsp;
+				<button type="button" class="btn btn-eyrc6">.btn-eyrc6</button>&nbsp;
+				<button type="button" class="btn btn-eyrc12">.btn-eyrc12</button></br></br>
+				<button type="button" class="btn btn-eyrc7">.btn-eyrc7</button>&nbsp;
+				<button type="button" class="btn btn-eyrc8">.btn-eyrc8</button>&nbsp;
+				<button type="button" class="btn btn-eyrc9">.btn-eyrc9</button></br></br>
+				<button type="button" class="btn btn-eyrc10">.btn-eyrc10</button>&nbsp;
+				<button type="button" class="btn btn-eyrc11">.btn-eyrc11</button>&nbsp;
 		</div>
+		<div class="col-md-4">
+			<h2>pannel</h2> 
+			<div class="bs-example" data-example-id="contextual-panels">
+				<div class="panel panel-eyrc1"> 
+					<div class="panel-heading"> 
+						 <h3 class="panel-title">panel-eyrc1</h3>
+					</div>
+					<div class="panel-body">
+						Panel content
+					</div>
+				</div>
+				<div class="panel panel-eyrc2"> 
+					<div class="panel-heading"> 
+						 <h3 class="panel-title">panel-eyrc2</h3>
+					</div>
+					<div class="panel-body">
+						Panel content
+					</div>
+				</div>
+				<div class="panel panel-eyrc3"> 
+					<div class="panel-heading"> 
+						 <h3 class="panel-title">panel-eyrc3</h3>
+					</div>
+					<div class="panel-body">
+						Panel content
+					</div>
+				</div>
+				<div class="panel panel-eyrc4"> 
+					<div class="panel-heading"> 
+						 <h3 class="panel-title">panel-eyrc4</h3>
+					</div>
+					<div class="panel-body">
+						Panel content
+					</div>
+				</div>
+				<div class="panel panel-eyrc5"> 
+					<div class="panel-heading"> 
+						 <h3 class="panel-title">panel-eyrc5</h3>
+					</div>
+					<div class="panel-body">
+						Panel content
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="col-md-4">
+			<h2>pannel</h2> 
+			<div class="bs-example" data-example-id="contextual-panels">
+				<div class="panel panel-eyrc6"> 
+					<div class="panel-heading"> 
+						 <h3 class="panel-title">panel-eyrc6</h3>
+					</div>
+					<div class="panel-body">
+						Panel content
+					</div>
+				</div>
+				<div class="panel panel-eyrc7"> 
+					<div class="panel-heading"> 
+						 <h3 class="panel-title">panel-eyrc7</h3>
+					</div>
+					<div class="panel-body">
+						Panel content
+					</div>
+				</div>
+				<div class="panel panel-eyrc8"> 
+					<div class="panel-heading"> 
+						 <h3 class="panel-title">panel-eyrc8</h3>
+					</div>
+					<div class="panel-body">
+						Panel content
+					</div>
+				</div>
+				<div class="panel panel-eyrc9"> 
+					<div class="panel-heading"> 
+						 <h3 class="panel-title">panel-eyrc9</h3>
+					</div>
+					<div class="panel-body">
+						Panel content
+					</div>
+				</div>
+				<div class="panel panel-eyrc10"> 
+					<div class="panel-heading"> 
+						 <h3 class="panel-title">panel-eyrc10</h3>
+					</div>
+					<div class="panel-body">
+						Panel content
+					</div>
+				</div>
+			</div>
+		</div>	
 	</div>
 </body>
 </html>
